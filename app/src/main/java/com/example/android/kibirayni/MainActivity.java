cd862f904cf323ea81cc288223fd44b5ea7d4900
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-25 23:21:43 +0530

    1 ChefRegistration.java signup onclick added in main false removed

--- app/src/main/java/com/example/android/kibirayni/MainActivity.java
+++ app/src/main/java/com/example/android/kibirayni/MainActivity.java
@@ -9,17 +9,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.os.Handler;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.circularreveal.CircularRevealHelper;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
@@ -29,15 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] Odisha = {"Bhubaneswar" , "Balasore" , "Cuttack" , "Puri"};
-        TextInputLayout Fname , Lname , Email , Pass , cpass , mobileno , houseno , area , pincode;
-        Spinner Statespin , Cityspin;
-        Button signup, Emaill , Phone;
-        CountryCodePicker Cpp;
-        FirebaseAuth FAuth;
-        DatabaseReference databaseReference;
-        FirebaseDatabase firebaseDatabase;
-        String fname,lname,emailid,password,confpassword,mobile,house,Area,Pincode,role="Chef",statee , cityy;
+
 
         imageView =  findViewById(R.id.imageView);
         textView =  findViewById(R.id.textView7);
